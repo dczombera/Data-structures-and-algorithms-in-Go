69f0595870cc8e94c12c488d0b5b666e1d7432c7
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-10-05 21:16:49 +0200

    Add main package to manually test data structures

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+
+	"github.com/dczomber/data-structures-and-algs/datastructs/node"
+	"github.com/dczomber/data-structures-and-algs/datastructs/queue"
+	"github.com/dczomber/data-structures-and-algs/datastructs/stack"
+)
+
+var nToPush = 6
+var nToPop = 3
+
+func main() {
+	printMsg("Stack")
+	s := stack.NewEmptyStack()
+	for i := 0; i < nToPush; i++ {
+		s.Push(node.Item(i))
+	}
+	for i := 0; i < nToPop; i++ {
+		s.Pop()
+	}
+
+	fmt.Printf("\nThere are %d nodes on the stack:\n", s.Size)
+	for !s.IsEmpty() {
+		item, _ := s.Pop()
+		fmt.Printf("%v\n", item)
+	}
+
+	printMsg("Queue")
+	q := queue.NewEmptyQueue()
+	for i := 0; i < nToPush; i++ {
+		q.Push(node.Item(i))
+	}
+	for i := 0; i < nToPop; i++ {
+		q.Pop()
+	}
+
+	fmt.Printf("\nThere are %d nodes on the queue:\n", q.Size)
+	for !q.IsEmpty() {
+		item, _ := q.Pop()
+		fmt.Printf("%v\n", item)
+	}
+}
+
+func printMsg(name string) {
+	del := fmt.Sprintf("*****%s*******", strings.Repeat("*", utf8.RuneCountInString(name)))
+	fmt.Printf("\n%s\n", del)
+	fmt.Printf("*\t%s\t*\n", name)
+	fmt.Println(del)
+}
