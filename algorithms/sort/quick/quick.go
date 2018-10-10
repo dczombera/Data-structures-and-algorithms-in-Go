7beb3e8686661a929394c7eaeb1c3d836f265ce4
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-10-10 11:50:46 +0200

    Implement quick sort algorithm

---
+++ algorithms/sort/quick/quick.go
@@ -0,0 +1,65 @@
+package quick
+
+type Key int
+
+func (this Key) Compare(that Key) int {
+	if this > that {
+		return 1
+	} else if this < that {
+		return -1
+	}
+	return 0
+}
+
+func Sort(kk []Key) {
+	sort(kk, 0, len(kk)-1)
+}
+
+func sort(kk []Key, lo, hi int) {
+	if hi <= lo {
+		return
+	}
+
+	j := partition(kk, lo, hi)
+	sort(kk, lo, j-1)
+	sort(kk, j+1, hi)
+}
+
+func partition(kk []Key, lo, hi int) int {
+	v := kk[lo]
+	i := lo
+	j := hi + 1
+	for true {
+		for {
+			i++
+			if !less(kk[i], v) || i == hi {
+				break
+			}
+		}
+
+		for {
+			j--
+			if !less(v, kk[j]) || j == lo {
+				break
+			}
+		}
+
+		// Check if pointer have crossed
+		if i >= j {
+			break
+		}
+
+		exch(kk, i, j)
+	}
+
+	exch(kk, lo, j)
+	return j
+}
+
+func exch(kk []Key, i, j int) {
+	kk[i], kk[j] = kk[j], kk[i]
+}
+
+func less(i, j Key) bool {
+	return i.Compare(j) < 0
+}
