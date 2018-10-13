db6a1253a0863244c128b486ce58a3473ec9e112
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-10-13 19:09:28 +0200

    Write additional unit tests for binary search

--- algorithms/search/binary_search/binary_search_test.go
+++ algorithms/search/binary_search/binary_search_test.go
@@ -1,7 +1,6 @@
 package binary_search
 
 import (
-	"log"
 	"testing"
 )
 
@@ -18,19 +17,21 @@ type testCase struct {
 var testCases = []testCase{
 	{[]Node{{1, "hola"}, {2, "hello"}}, []Node{{1, "hola"}, {2, "hello"}}},
 	{[]Node{{1, "hola"}, {2, "hello"}, {1, "bye"}, {42, "hola"}}, []Node{{42, "hola"}, {1, "bye"}, {2, "hello"}}},
-	{[]Node{}, []Node{}},
 }
 var errorTestCases = []testCase{
 	{[]Node{{1, "hola"}, {2, "hello"}}, []Node{{5, ""}, {42, ""}}},
 }
 
-func TestBinarySearch(t *testing.T) {
+var emptyTestCases = []testCase{
+	{[]Node{}, []Node{}},
+}
+
+func TestBinarySearchPut(t *testing.T) {
 	for _, tc := range testCases {
 		bs := NewEmptyBinarySearchST()
 		for _, n := range tc.data {
 			bs.Put(n.Key, n.Val)
 		}
-		log.Println(bs)
 		for _, w := range tc.want {
 			v, err := bs.Get(w.Key)
 			if err != nil {
@@ -39,6 +40,48 @@ func TestBinarySearch(t *testing.T) {
 			if v != w.Val {
 				t.Errorf("Got %v, want %v", v, w.Val)
 			}
+		}
+	}
+}
+
+func TestBinarySearchEmptyPut(t *testing.T) {
+	for _, tc := range emptyTestCases {
+		bs := NewEmptyBinarySearchST()
+		for _, n := range tc.data {
+			bs.Put(n.Key, n.Val)
+		}
+		if bs.Size() != 0 {
+			t.Errorf("Got %d, want %d", bs.Size(), 0)
+		}
+	}
+}
+
+func TestErrorBinarySearch(t *testing.T) {
+	for _, tc := range errorTestCases {
+		bs := NewEmptyBinarySearchST()
+		for _, n := range tc.data {
+			bs.Put(n.Key, n.Val)
+		}
+		for _, w := range tc.want {
+			_, err := bs.Get(w.Key)
+			if err == nil {
+				t.Errorf("Got %v, want error message", err)
+			}
+		}
+	}
+}
+
+func TestBinarySearchDelete(t *testing.T) {
+	for _, tc := range testCases {
+		bs := NewEmptyBinarySearchST()
+		for _, n := range tc.data {
+			bs.Put(n.Key, n.Val)
+		}
+		bs.Delete(Key(tc.data[0].Key))
+		bs.Delete(tc.data[len(tc.data)-1].Key)
+		want := len(tc.want) - 2
+		if bs.Size() != want {
+			t.Errorf("Got %d, want %d", bs.Size(), want)
 		}
 	}
 }
