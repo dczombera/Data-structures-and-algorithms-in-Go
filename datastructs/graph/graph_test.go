e44b43ac5f95ef71e074e61e46dc8b0ddbc9d247
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-10-30 12:02:04 +0100

    Fix failing graph test

--- datastructs/graph/graph_test.go
+++ datastructs/graph/graph_test.go
@@ -20,7 +20,7 @@ type VertexDegree struct {
 }
 
 var testCases = []testCase{
-	{7, [][]int{[]int{0, 1}, []int{2, 4}, []int{1, 5}, []int{7, 5}, []int{7, 1}}, []VertexEdges{{0, []Value{1}}, {1, []Value{7, 5, 0}}, {2, []Value{4}}, {4, []Value{2}}, {5, []Value{7, 1}}, {7, []Value{1, 5}}}, []VertexDegree{{0, 1}, {1, 3}, {2, 1}, {4, 1}, {5, 2}, {7, 2}}},
+	{8, [][]int{[]int{0, 1}, []int{2, 4}, []int{1, 5}, []int{7, 5}, []int{7, 1}}, []VertexEdges{{0, []Value{1}}, {1, []Value{7, 5, 0}}, {2, []Value{4}}, {4, []Value{2}}, {5, []Value{7, 1}}, {7, []Value{1, 5}}}, []VertexDegree{{0, 1}, {1, 3}, {2, 1}, {4, 1}, {5, 2}, {7, 2}}},
 }
 
 func containsVertices(has Bag, want []Value) bool {
