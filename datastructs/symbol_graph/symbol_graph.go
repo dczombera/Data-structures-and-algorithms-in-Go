f574a4f98f606bb78c7f5b5980a0fd981db85dae
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-11-05 16:05:50 +0100

    Implement symbol graph data type that uses strings as vertex names for an undirected graph

---
+++ datastructs/symbol_graph/symbol_graph.go
@@ -0,0 +1,84 @@
+package symbol_graph
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/dczombera/data-structures-and-algorithms-in-go/datastructs/graph"
+)
+
+type SymbolGraph struct {
+	keys        map[string]int
+	reverseKeys []string
+	graph       *graph.Graph
+}
+
+var initCap = 16
+
+func NewSymbolGraph(filename string, delimiter string) *SymbolGraph {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	sg := SymbolGraph{make(map[string]int), nil, nil}
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		tokens := strings.Split(scanner.Text(), delimiter)
+		for _, t := range tokens {
+			sg.keys[t] = len(sg.keys)
+		}
+	}
+
+	sg.reverseKeys = make([]string, len(sg.keys))
+	for k, v := range sg.keys {
+		sg.reverseKeys[v] = k
+	}
+
+	g := graph.NewGraph(len(sg.keys))
+	// Rewind reader in order to avoid creation of new scanner
+	f.Seek(0, 0)
+	// connect first vertex on each line with all other on same line
+	for scanner.Scan() {
+		tokens := strings.Split(scanner.Text(), delimiter)
+		v := sg.keys[tokens[0]]
+		for _, t := range tokens[1:] {
+			g.AddEdge(v, sg.keys[t])
+		}
+	}
+
+	return &sg
+}
+
+func (sg *SymbolGraph) contains(s string) bool {
+	_, ok := sg.keys[s]
+	return ok
+}
+
+func (sg *SymbolGraph) indexOf(s string) (int, error) {
+	i, ok := sg.keys[s]
+	if !ok {
+		return -1, errors.New(fmt.Sprintf("vertex with name %v does not exist", s))
+	}
+	return i, nil
+}
+
+func (sg *SymbolGraph) nameOf(i int) string {
+	sg.validateVertex(i)
+	return sg.reverseKeys[i]
+}
+
+func (sg *SymbolGraph) Graph() *graph.Graph {
+	return sg.graph
+}
+
+func (sg *SymbolGraph) validateVertex(v int) {
+	if v < 0 || v >= sg.graph.Vertices() {
+		panic(fmt.Sprintf("Vertex %v out of bounds", v))
+	}
+}
