88736d2f95d48d282c8c55fdfd9b72839bdee12f
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-11-13 22:43:25 +0100

    Implement edge struct used in edge weighted graph data type

---
+++ algorithms/graph/edge/edge.go
@@ -0,0 +1,42 @@
+package edge
+
+import (
+	"fmt"
+)
+
+type Edge struct {
+	eitherVertex int
+	otherVertex  int
+	weight       float64
+}
+
+func NewEdge(v, w int, weight float64) Edge {
+	return Edge{v, w, weight}
+}
+
+func (e Edge) Either() int {
+	return e.eitherVertex
+}
+
+func (e Edge) Other(vertex int) int {
+	if vertex == e.eitherVertex {
+		return e.otherVertex
+	} else if vertex == e.otherVertex {
+		return e.eitherVertex
+	}
+
+	panic(fmt.Sprintf("Vertex %v is not a valid vertex for this edge", vertex))
+}
+
+func (e Edge) Weight() float64 {
+	return e.weight
+}
+
+func (this Edge) Compare(other Edge) int {
+	if this.weight > other.weight {
+		return 1
+	} else if this.weight < other.weight {
+		return -1
+	}
+	return 0
+}
