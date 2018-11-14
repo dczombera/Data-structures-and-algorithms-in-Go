ed431eed4451f2267799b56120ca2bf4468da398
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-11-15 00:29:01 +0100

    Write tests for minimum priority queue

---
+++ algorithms/graph/minimum_spanning_tree/min_priority_queue_test.go
@@ -0,0 +1,45 @@
+package minimum_spanning_tree
+
+import (
+	"testing"
+
+	"github.com/dczombera/data-structures-and-algorithms-in-go/datastructs/edge_weighted_graph/edge"
+)
+
+type testCase struct {
+	data []edge.Edge
+	want []edge.Edge
+}
+
+var testCases = []testCase{
+	{[]edge.Edge{edge.NewEdge(0, 1, 2.3), edge.NewEdge(2, 3, 0.42), edge.NewEdge(1, 2, 13.37)}, []edge.Edge{edge.NewEdge(2, 3, 0.42), edge.NewEdge(0, 1, 2.3), edge.NewEdge(1, 2, 13.37)}},
+	{[]edge.Edge{edge.NewEdge(0, 1, 0.0), edge.NewEdge(3, 8, -2.3), edge.NewEdge(4, 5, 2.3), edge.NewEdge(5, 6, -2.3)}, []edge.Edge{edge.NewEdge(3, 8, -2.3), edge.NewEdge(5, 6, -2.3), edge.NewEdge(0, 1, 0.0), edge.NewEdge(4, 5, 2.3)}},
+}
+
+func TestMinPriorityQueue(t *testing.T) {
+	pq := NewMinPriorityQueue()
+	for _, tc := range testCases {
+		for _, e := range tc.data {
+			pq.Insert(e)
+		}
+
+		if pq.Size() != len(tc.data) {
+			t.Errorf("Got size %d, want %d", len(tc.data), pq.Size())
+		}
+
+		for _, e := range tc.want {
+			min, _ := pq.DelMin()
+			if min != e {
+				t.Errorf("Got %v, want %v", min, e)
+			}
+		}
+	}
+}
+
+func TestMinPriorityQueueError(t *testing.T) {
+	pq := NewMinPriorityQueue()
+	_, e := pq.DelMin()
+	if e == nil {
+		t.Errorf("Got %v, want error message", e)
+	}
+}
