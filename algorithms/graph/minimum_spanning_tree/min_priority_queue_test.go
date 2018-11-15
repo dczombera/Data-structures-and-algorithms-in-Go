14710e1308262d61f85e5766d02fa485f692166a
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-11-15 17:44:50 +0100

    Avoid naming conflict in package

--- algorithms/graph/minimum_spanning_tree/min_priority_queue_test.go
+++ algorithms/graph/minimum_spanning_tree/min_priority_queue_test.go
@@ -6,17 +6,17 @@ import (
 	"github.com/dczombera/data-structures-and-algorithms-in-go/datastructs/edge_weighted_graph/edge"
 )
 
-type testCase struct {
+type testCaseQueue struct {
 	data []edge.Edge
 	want []edge.Edge
 }
 
-var testCases = []testCase{
-	{[]edge.Edge{edge.NewEdge(0, 1, 2.3), edge.NewEdge(2, 3, 0.42), edge.NewEdge(1, 2, 13.37)}, []edge.Edge{edge.NewEdge(2, 3, 0.42), edge.NewEdge(0, 1, 2.3), edge.NewEdge(1, 2, 13.37)}},
-	{[]edge.Edge{edge.NewEdge(0, 1, 0.0), edge.NewEdge(3, 8, -2.3), edge.NewEdge(4, 5, 2.3), edge.NewEdge(5, 6, -2.3)}, []edge.Edge{edge.NewEdge(3, 8, -2.3), edge.NewEdge(5, 6, -2.3), edge.NewEdge(0, 1, 0.0), edge.NewEdge(4, 5, 2.3)}},
-}
-
 func TestMinPriorityQueue(t *testing.T) {
+	testCases := []testCaseQueue{
+		{[]edge.Edge{edge.NewEdge(0, 1, 2.3), edge.NewEdge(2, 3, 0.42), edge.NewEdge(1, 2, 13.37)}, []edge.Edge{edge.NewEdge(2, 3, 0.42), edge.NewEdge(0, 1, 2.3), edge.NewEdge(1, 2, 13.37)}},
+		{[]edge.Edge{edge.NewEdge(0, 1, 0.0), edge.NewEdge(3, 8, -2.3), edge.NewEdge(4, 5, 2.3), edge.NewEdge(5, 6, -2.3)}, []edge.Edge{edge.NewEdge(3, 8, -2.3), edge.NewEdge(5, 6, -2.3), edge.NewEdge(0, 1, 0.0), edge.NewEdge(4, 5, 2.3)}},
+	}
+
 	pq := NewMinPriorityQueue()
 	for _, tc := range testCases {
 		for _, e := range tc.data {
