73c8c19b41801bab234d72b3e08910df4fa995b8
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-11-22 12:13:48 +0100

    Write client example of how to use the critical path method data type in order to solve the parallel precedence-constrained job scheduling problem

--- algorithms/graph/single_source_shortest_and_longest_paths_edge_weighted_directed_graph/critical_path_method_client_example/main.go
+++ algorithms/graph/single_source_shortest_and_longest_paths_edge_weighted_directed_graph/critical_path_method_client_example/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "flag"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/dczombera/data-structures-and-algorithms-in-go/algorithms/graph/single_source_shortest_and_longest_paths_edge_weighted_directed_graph/critical_path_method"
+)
 
 var file string
 
@@ -16,4 +25,27 @@ func init() {
 
 func main() {
 	flag.Parse()
+	cpm := critical_path_method.NewCPM(file)
+	f, err := os.Open(file)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	scanner := bufio.NewScanner(f)
+	if !scanner.Scan() {
+		log.Fatalln("EOF")
+	}
+
+	jobs, err := strconv.Atoi(scanner.Text())
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println("jobs\t\tstart\t\t finish")
+	fmt.Println("***************************************")
+	for i := 0; i < jobs; i++ {
+		fmt.Printf("%v\t\t%.1f\t\t%.1f\n", i, cpm.StartTimeOf(i), cpm.FinishTimeOf(i))
+	}
+	fmt.Println("***************************************")
+	fmt.Printf("Overall finish time: %.1f\n", cpm.OverallFinishTime())
 }
