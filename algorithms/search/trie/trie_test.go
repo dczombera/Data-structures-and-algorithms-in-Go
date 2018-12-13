b0d8e1a8d03cc66ef7258d1f460db9a4fa92c6f4
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-12-13 21:29:34 +0100

    Write tests for key collection methods of trie data structure

--- algorithms/search/trie/trie_test.go
+++ algorithms/search/trie/trie_test.go
@@ -1,6 +1,8 @@
 package trie
 
-import "testing"
+import (
+	"testing"
+)
 
 type testCase struct {
 	data []KV
@@ -52,6 +54,78 @@ func TestTrieError(t *testing.T) {
 			v, err := trie.Get(w.key)
 			if err == nil {
 				t.Errorf("key %v with value %v found in trie, don't want it to exist", w.key, v)
+			}
+		}
+	}
+}
+
+func TestCollecteys(t *testing.T) {
+	type Key struct {
+		pre  string
+		keys []string
+	}
+	testCases := []struct {
+		data []KV
+		want []Key
+	}{
+		{
+			data: []KV{{"she", 1}, {"sea", 1}, {"see", 1}, {"shell", 1}, {"shell", 1}, {"shellsort", 1}, {"hello", 1}, {"he", 1}},
+			want: []Key{{"she", []string{"she", "shell", "shellsort"}}, {"sea", []string{"sea"}}, {"shell", []string{"shell", "shellsort"}}, {"he", []string{"he", "hello"}}, {"hello", []string{"hello"}}, {"s", []string{"sea", "see", "she", "shell", "shellsort"}}},
+		},
+		{
+			data: []KV{{"Han", 1}, {"shot", 1}, {"first", 1}, {",", 1}, {"he", 1}, {"said", 1}, {".", 1}},
+			want: []Key{{"", []string{",", ".", "Han", "first", "he", "said", "shot"}}},
+		},
+	}
+
+	for _, tc := range testCases {
+		trie := Constructor()
+		for _, d := range tc.data {
+			trie.Put(d.key, d.val)
+		}
+
+		for _, w := range tc.want {
+			keys := trie.KeysWithPrefix(w.pre)
+			for i, k := range w.keys {
+				if keys[i] != k {
+					t.Errorf("Got key %v in collection of keys with prefix %v, want %v", keys[i], w.pre, k)
+				}
+			}
+		}
+	}
+}
+
+func TestKeysThatMatch(t *testing.T) {
+	type Key struct {
+		pattern string
+		keys    []string
+	}
+	testCases := []struct {
+		data []KV
+		want []Key
+	}{
+		{
+			data: []KV{{"she", 1}, {"sea", 1}, {"see", 1}, {"shell", 1}, {"shell", 1}, {"shellsort", 1}, {"hello", 1}, {"he", 1}},
+			want: []Key{{"s.e", []string{"see", "she"}}, {".ea", []string{"sea"}}, {".hel.", []string{"shell"}}, {"he", []string{"he"}}, {"he..o", []string{"hello"}}, {"...", []string{"sea", "see", "she"}}},
+		},
+		{
+			data: []KV{{"Han", 1}, {"shot", 1}, {"first", 1}, {",", 1}, {"he", 1}, {"said", 1}, {".", 1}},
+			want: []Key{{".", []string{",", "."}}},
+		},
+	}
+
+	for _, tc := range testCases {
+		trie := Constructor()
+		for _, d := range tc.data {
+			trie.Put(d.key, d.val)
+		}
+
+		for _, w := range tc.want {
+			keys := trie.KeysThatMatch(w.pattern)
+			for i, k := range w.keys {
+				if keys[i] != k {
+					t.Errorf("Got key %v in collection of keys that match %v, want %v", keys[i], w.pattern, k)
+				}
 			}
 		}
 	}
