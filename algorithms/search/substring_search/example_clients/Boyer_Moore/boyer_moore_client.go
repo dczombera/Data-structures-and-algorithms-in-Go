1ef11ece96a47809bd606d6535e476f5abc7820d
Dennis Czombera <milchschaumschluerfer@gmail.com>
2018-12-22 16:30:46 +0100

    Write example client for the Boyer-Moore substring search algorithm

---
+++ algorithms/search/substring_search/example_clients/Boyer_Moore/boyer_moore_client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	substring_search "github.com/dczombera/data-structures-and-algorithms-in-go/algorithms/search/substring_search"
+)
+
+var text string
+var pattern string
+
+func init() {
+	const (
+		defaultText    = ""
+		usageText      = "Text used for to search for given pattern"
+		defaultPattern = ""
+		usagePattern   = "Pattern used to find substring in text"
+	)
+
+	flag.StringVar(&text, "text", defaultText, usageText)
+	flag.StringVar(&text, "t", defaultText, usageText+" (shorthand)")
+	flag.StringVar(&pattern, "pattern", defaultPattern, usagePattern)
+	flag.StringVar(&pattern, "p", defaultPattern, usagePattern+" (shorthand)")
+}
+func main() {
+	flag.Parse()
+	bm := substring_search.BoyerMooreConstructor(pattern)
+	pos := bm.Search(text)
+	if pos == len(text) {
+		fmt.Printf("No substring for pattern %v found in text %v\n", pattern, text)
+	} else {
+		fmt.Println("Text: \t", text)
+		fmt.Print("Pattern: ")
+		for i := 0; i < pos; i++ {
+			fmt.Print(" ")
+		}
+		fmt.Println(pattern)
+	}
+}
